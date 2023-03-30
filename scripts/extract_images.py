b48c87140d2f2005dddc6d9bc781177d514ceadd
djadkin <djadkin@kth.se>
2023-03-30 12:44:48 +0200

    cleanup

--- scripts/extract_images.py
+++ scripts/extract_images.py
@@ -6,7 +6,6 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 import math
 from skimage import io
-from IPython.display import display, clear_output
 from pathlib import Path
 from kitti_utils import get_data, get_object_centers, \
 get_point_idxs, point_in_cube, load_kitti_calib, \
@@ -26,7 +25,12 @@ lim_h = 128
 lim_w = 128
 
 dicts_path = os.path.join(OUTPUT_DIR, 'data_dicts')
-children_output_paths = ['data_dicts', 'image_2', 'point_clouds', 'centers', 'calib']
+children_output_paths = ['data_dicts', 
+                         'image_2', 
+                         'point_clouds', 
+                         'centers', 
+                         'calib'
+                        ]
 
 for child_path in children_output_paths:
     out_path = Path(os.path.join(OUTPUT_DIR, child_path))
@@ -39,9 +43,13 @@ def crop_objects(save_dicts = True, save_objects=True):
     """
     out_id = 0
     for file_id in tqdm.tqdm(range(0, 7481)):
-        data = get_data(file_id, IMG_DIR, LABEL_DIR, POINT_CLOUD_DIR, CALIB_DIR)
+        data = get_data(file_id, 
+                        IMG_DIR, 
+                        LABEL_DIR, 
+                        POINT_CLOUD_DIR, 
+                        CALIB_DIR
+                       )
         if save_dicts:
-
             DICTS_DIR = os.path.join(OUTPUT_DIR,
                                      'data_dicts/{0:06d}.bin'.format(file_id)
                                     )
@@ -74,30 +82,47 @@ def crop_objects(save_dicts = True, save_objects=True):
                     img_h = image.shape[0]
                     img_w = image.shape[1]
                     
-                    if (int(c_h-lim_h) > 0 and int(c_h+lim_h) < img_h and
-                        int(c_w-lim_w) > 0 and int(c_w+lim_w) < img_w) and idxs.shape[0]:
-                        clear_output(wait=True)
-                        pc = data['point_cloud']
+                    if (int(c_h - lim_h) > 0 
+                        and int(c_h + lim_h) < img_h 
+                        and int(c_w - lim_w) > 0 
+                        and int(c_w + lim_w) < img_w 
+                        and idxs.shape[0]):
                         
+                        pc = data['point_cloud']
                         # save partial point cloud
                         pc_partial = pc[idxs, :]
-                        pc_partial = pc_partial.reshape(pc_partial.shape[0]*pc_partial.shape[1])
-                        pc_partial.tofile(os.path.join(OUTPUT_DIR, 'point_clouds/{0:06d}.bin'.format(out_id)))
-
+                        pc_partial_len = pc_partial.shape[0]*pc_partial.shape[1]
+                        pc_partial = pc_partial.reshape(pc_partial_len)
+                        file_rel_dir = 'point_clouds/{0:06d}.bin'.format(out_id)
+                        filename_out = os.path.join(OUTPUT_DIR, 
+                                                       file_rel_dir
+                                                      )
+                        pc_partial.tofile(filename_out)
                         # save partial image
-                        img_partial = image[int(c_h-lim_h):int(c_h+lim_h), int(c_w-lim_w):int(c_w+lim_w)]
-                        
+                        img_partial = image[int(c_h-lim_h):int(c_h+lim_h), 
+                                            int(c_w-lim_w):int(c_w+lim_w)]
                         assert img_partial.shape == (256, 256, 3) 
-                        
                         # save cropped image to png
-                        io.imsave(os.path.join(OUTPUT_DIR, 'image_2/{0:06d}.png'.format(out_id)), img_partial)
-
-                        np.save(os.path.join(OUTPUT_DIR, 'centers/{0:06d}.npy'.format(out_id)), data['obj_centers'][L])
-
-                        with open(os.path.join(CALIB_DIR, '{0:06d}.txt'.format(file_id)), 'r') as f:
-                            g = open(os.path.join(OUTPUT_DIR, 'calib/{0:06d}.txt'.format(out_id)), "x")
-                            for line in f.readlines():
-                                g.write(line)
+                        file_rel_dir = 'image_2/{0:06d}.png'.format(out_id)
+                        filename_out = os.path.join(OUTPUT_DIR, file_rel_dir)
+                        io.imsave(filename_out, 
+                                  img_partial
+                                 )
+                        # save cropped point cloud
+                        file_rel_dir = 'centers/{0:06d}.npy'.format(out_id)
+                        filename_out = os.path.join(OUTPUT_DIR, file_rel_dir)
+                        np.save(filename_out, 
+                                data['obj_centers'][L]
+                               )
+                        # save calib file
+                        file_rel_dir_in = '{0:06d}.txt'.format(file_id)
+                        filename_in = os.path.join(CALIB_DIR, file_rel_dir_in)
+                        file_rel_dir_out = 'calib/{0:06d}.txt'.format(out_id)
+                        filename_out = os.path.join(OUTPUT_DIR, file_rel_dir_out)
+                        with open(filename_in, 'r') as f: 
+                            with open(filename_out, "x") as g:
+                                for line in f.readlines():
+                                    g.write(line)
                         out_id += 1
 
 if __name__ == "__main__":
